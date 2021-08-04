7e60186297b21f99c1e824b3d5b571787d8e7cfb
Курцева Н.О <kurzevano@gmail.com>
2021-08-04 16:26:54 +0700

    + Добавлены модели данных

---
+++ FinancialAccountService/Models/Balance.cs
@@ -0,0 +1,13 @@
+﻿namespace FinancialAccountService.Models
+{
+    /// <summary>
+    /// Текущий баланс пользователя
+    /// </summary>
+    public class Balance
+    {
+        /// <summary>
+        /// Текущий остаток на счёте
+        /// </summary>
+        public decimal Summ { get; set; }
+    }
+}
---
+++ FinancialAccountService/Models/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FinancialAccountService.Models
+{
+    /// <summary>
+    /// Пользователь
+    /// </summary>
+    public class User
+    {
+        /// <summary>
+        /// Идентификатор
+        /// </summary>
+        public int UserId { get; set; }
+
+        /// <summary>
+        /// Фамилия
+        /// </summary>
+        public string LastName { get; set; }
+        
+        /// <summary>
+        /// Имя
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// Отчество
+        /// </summary>
+        public string MiddleName { get; set; }
+
+        /// <summary>
+        /// Дата рождения
+        /// </summary>
+        public DateTime DateBirth { get; set; }
+
+        /// <summary>
+        /// Текущий баланс пользователя
+        /// </summary>
+        public Balance CurrentBalance { get; set; }
+
+        /// <summary>
+        /// История операций на счёте пользователя
+        /// </summary>
+        public List<BalanceTransaction> BalanceTransactions { get; set; }
+    }
+}
