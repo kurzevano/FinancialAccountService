3e0142235fecabab2dc0f2c7eef08816af1d99ce
Курцева Н.О <kurzevano@gmail.com>
2021-08-08 21:30:45 +0700

    + Added test with multiple DbContext - Removed test with lock object

---
+++ FinancialAccount.Tests/TestUtils.cs
@@ -0,0 +1,34 @@
+﻿using FinancialAccountService.Model;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinancialAccount.Tests
+{
+    internal static class TestUtils
+    {
+        internal const int FakeUsersCount = 50;
+
+        /// <summary>
+        /// Создаёт список пользователей для теста
+        /// </summary>
+        /// <returns></returns>
+        internal static List<User> GetFakeUsers()
+        {
+            var users = new List<User>();
+            for (int i = 0; i < FakeUsersCount; i++)
+            {
+                users.Add(new User
+                {
+                    Id = i + 1,
+                    FirstName = $"Name{i}",
+                    LastName = $"Lastaname{i}",
+                    MiddleName = $"Lastaname{i}"
+                });
+            }
+
+            return users;
+        }
+    }
+}
